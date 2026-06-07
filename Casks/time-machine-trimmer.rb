d4d87ec4cbe3a73fc6051077076c05517e0b3026
Ricardo Leal <ricardo.martins-leal@siemens.com>
2026-06-07 17:50:29 +0100

    Update SHA256 for v1.0.0

--- Casks/time-machine-trimmer.rb
+++ Casks/time-machine-trimmer.rb
@@ -1,6 +1,6 @@
 cask "time-machine-trimmer" do
   version "1.0.0"
-  sha256 ""
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/ricardoleal/TimeMachineTrimmer/releases/download/#{version}/TimeMachineTrimmer.dmg"
   name "TimeMachineTrimmer"
